27097ff768bae289bfe9e09322177f57af6b2eb3
jamielaff <jamielafferty99@gmail.com>
2020-05-01 14:29:40 +0100

    Update to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_13_194023) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
