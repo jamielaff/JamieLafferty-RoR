afc4dfe4245632a140b328910019762cc5b0987c
jamielaff <jamielafferty99@gmail.com>
2020-05-05 21:20:51 +0100

    Remove redundant page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
