8b533c4996513323b93d45f2ddfa4598d9c8d8c0
Jamie Lafferty <jamielafferty99@gmail.com>
2020-05-05 22:36:13 +0200

    Article action restricted to article owner, refactor to partials, rework authorisation catches
    * Render article edit & delete controls in a reusable partial
    
    * General tidy up
    
    * Restrict edit, delete and update actions of an article to only the article owner
    
    * We no speak Americano
    
    * Redesign article ownership check, offload logic to model instead of controller
    
    * Corrected extension
    
    * Use new owned by boolean
    
    * Use session present check instead of double negative
    
    * Remove redundant page
    
    * Change authorisation checks, now whitelists actions instead of blacklisting
    
    * Pluralise before action for article actions
    
    * Remove welcome from skip before actions (it does not exist)
    
    * Conform to standards, more root to end of routes file

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   belongs_to :user
+  
+  def owned_by?(user_to_verify)
+    user == user_to_verify
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -10,4 +8,5 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
 
+  root 'pages#index'
 end
