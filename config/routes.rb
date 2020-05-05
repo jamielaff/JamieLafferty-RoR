85c474033d9e1660bdcde429ce6adf8ca4667f1f
jamielaff <jamielafferty99@gmail.com>
2020-05-05 21:34:08 +0100

    Conform to standards, more root to end of routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -9,4 +8,5 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
 
+  root 'pages#index'
 end
