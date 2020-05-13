3e894649477a7de3f93fedea02fb9f3d7ef76bb8
jamielaff <jamielafferty99@gmail.com>
2020-05-13 18:41:54 +0100

    Change subdomain to blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  constraints subdomain: 'ruby' do
+  constraints subdomain: 'blog' do
     get 'signup',     to: 'users#new'
     get 'login',      to: 'sessions#new'
     post 'login',     to: 'sessions#create'
     delete 'logout',  to: 'sessions#destroy'
-    get '/',          to: 'pages#ruby_index'
+    get '/',          to: 'pages#blog_index'
 
     resources :articles
     resources :users, except: [:new]
