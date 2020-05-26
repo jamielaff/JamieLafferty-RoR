218debcbf2bec29f738f30a452383eaf1acf99f8
jamielaff <jamielafferty99@gmail.com>
2020-05-26 22:11:17 +0100

    Order homepage articles by ascending date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
 	skip_before_action :authorised, only: [:index]
 
 	def index
-    @articles = Article.by_admin.paginate(page: params[:page], per_page: 5)
+    @articles = Article.by_admin.paginate(page: params[:page], per_page: 5).order(created_at: :asc)
 	end
 end
