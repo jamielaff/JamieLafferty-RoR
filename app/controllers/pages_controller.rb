5bb8444dfae647c9f16231accf601f33a61b7d39
jamielaff <jamielafferty99@gmail.com>
2020-05-26 21:36:37 +0100

    Corrected spacing on controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
 	skip_before_action :authorised, only: [:index]
 
 	def index
-		@articles = Article.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+	  @articles = Article.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
 	end
 end
