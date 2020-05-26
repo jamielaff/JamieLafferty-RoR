c28efb9588af781a97a14ea22c09bcb869c6c446
Jamie Lafferty <jamielafferty99@gmail.com>
2020-05-26 21:41:21 +0100

    Scope & paginated admin articles
    * Added pry gem for debugging
    
    * Added scope by_admin to index.  
    This will show only admin posts on the home page, for the living TIL.  
    Paginated
    
    * Updated navbar name
    
    * Added a better description about the site, about me & about the purpose of the project
    
    * Corrected spacing on controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ActiveRecord::Base
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   belongs_to :user
   
+  scope :by_admin, -> { joins(:user).where('users.is_admin' => 'true') }
+
   def owned_by?(user_to_verify)
     user == user_to_verify
   end
